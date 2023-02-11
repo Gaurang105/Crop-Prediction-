b3d0e46685a2318b8e9375a168f09a45f89a9b45
Gaurang105 <gujratigaurang@gmail.com>
2023-02-11 13:09:45 +0530

    html js updates

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const nextStepButton = document.getElementById('next-step');
+const nextStepButton = document.getElementById('submit');
 const backStepButton = document.getElementById('back-step');
 const container = document.getElementById('container');
 
@@ -9,3 +9,33 @@ nextStepButton.addEventListener('click', () => {
 backStepButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function submitData(){
+	let nin = document.getElementById('n')
+	let poh = document.getElementById('p')
+	let pot = document.getElementById('k')
+	let tem = document.getElementById('temp')
+	let hum = document.getElementById('humidity')
+	let PH = document.getElementById('ph')
+	let rains = document.getElementById('rain')
+
+	console.log()
+
+	jsonObject={
+		"N": nin.value,
+		"P": poh.value,
+		"K": pot.value,
+		"temp": tem.value,
+		"humidity": hum.value,
+		"nature": "acidic",
+		"rainfall": rains.value
+	}
+	let config = {
+		params: jsonObject
+	  }
+	  
+	  axios.get('http://127.0.0.1:8000/api/cropr', config).then(function(response) {
+		  console.log(response);
+	  })
+
+}
