96974590deae1a648caaf37df40d067d1c28bd2d
Gaurang105 <gujratigaurang@gmail.com>
2023-02-11 11:26:45 +0530

    js updates

--- client/script.js
+++ client/script.js
@@ -5,13 +5,6 @@ const container = document.getElementById('container');
 nextStepButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
-const nextStepButton = document.getElementById('next-step');
-const backStepButton = document.getElementById('back-step');
-const container = document.getElementById('container');
-
-nextStepButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
 backStepButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
